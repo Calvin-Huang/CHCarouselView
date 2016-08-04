3317e5ea69009fa7cccb4205a2c1ebe44f7c1eea
Calvin <calvin.peak@capslock.tw>
2016-08-05 04:09:05 +0900

    Add CarouselView file into project.

---
+++ CHCarouselView/Views/CarouselView.swift
@@ -0,0 +1,21 @@
+//
+//  CarouselView.swift
+//  CHCarouselView
+//
+//  Created by Calvin on 8/5/16.
+//  Copyright © 2016 CapsLock. All rights reserved.
+//
+
+import UIKit
+
+class CarouselView: UIScrollView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
