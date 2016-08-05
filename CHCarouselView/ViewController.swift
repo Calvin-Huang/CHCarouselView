6bb8f667194e4aaab8f4344ab822f4c9cf1221bd
Calvin <calvin.peak@capslock.tw>
2016-08-05 20:42:58 +0900

    - Change example carousel size in storyboard. - Complete example code for how to use carousel.

--- CHCarouselView/ViewController.swift
+++ CHCarouselView/ViewController.swift
@@ -7,19 +7,30 @@
 //
 
 import UIKit
+import Kingfisher
 
 class ViewController: UIViewController {
+    @IBOutlet weak var carouselView: CarouselView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let imageUrls = [
+            "https://dl.dropboxusercontent.com/u/108987767/Meme/01_Cupido.jpg",
+            "https://dl.dropboxusercontent.com/u/108987767/Meme/bugs_everywhere.png",
+            "https://dl.dropboxusercontent.com/u/108987767/Meme/p56an6SZj5_d.gif",
+            "https://dl.dropboxusercontent.com/u/108987767/Meme/1557643_489616404482423_780202608_n.jpg",
+        ]
+        
+        carouselView.views = imageUrls
+                                .map { url -> UIImageView in
+                                    let imageView = UIImageView()
+                                    imageView.kf_setImageWithURL(NSURL(string: url)!)
+                                    imageView.contentMode = .ScaleAspectFill
+                                    imageView.clipsToBounds = true
+                                    
+                                    return imageView
+                                }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
